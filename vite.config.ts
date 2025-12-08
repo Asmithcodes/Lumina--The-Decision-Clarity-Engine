d09015a4accafd5b3dfe5bee72cc6e189ba28a97
Asmith Pampana <pampana.asmith@gmail.com>
2025-12-09 00:33:01 +0530

    Fix-base-path

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/lumina_-decision-clarity-engine/',
+  base: '/Lumina--The-Decision-Clarity-Engine/',
   define: {
     'process.env': {}
   }
